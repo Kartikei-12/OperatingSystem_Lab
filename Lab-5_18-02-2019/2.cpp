d5c889a898333ff5f0aca3e8df9b0fa9cdf2322e
Kartikei-12 <kartikeimittal@gmail.com>
2019-02-18 10:51:08 +0530

    Implmented brst time prediction technique

--- Lab-5_18-02-2019/2.cpp
+++ Lab-5_18-02-2019/2.cpp
@@ -1,5 +1,7 @@
 // @author: Kartikei Mittal
 // ID: 2017KUCP1032
+// Implemented Sortest Job First Scheduling Algorithm
+// with burst time prediction
 #include<iostream>
     using namespace std;
 
@@ -8,21 +10,45 @@ struct Process {
     int at, bt, ct, tat, wt;
     double pbt;
 };
+
+int getShotestArrivedProcess(Process P[], int size, int timE)
+{
+    int a = -1, min=32000;
+    for(int i=0; i<size; ++i)
+        if(P[i].pbt<min && !P[i].isCompleted && P[i].at<=timE)
+        {
+            a = i;
+            min = P[i].pbt;
+        }
+    return a;
+}
+
+bool isProcessRemaining(Process P[], int size)
+{
+    for(int i=0; i<size; ++i)
+        if(!P[i].isCompleted)
+            return true;
+    return false;
+}
+
 void showProcessTab(Process P[], int size)
 {
-    cout<<"\n\nProcess Id | AT   | BT   | PBT"<<endl;
+    cout<<"\n\nProcess Id | AT   | BT | PBT | CT | TAT   | WT   |"<<endl;
     for(int i=0; i<size; ++i)
         cout<<"\t"
-            <<i+1<<"       "
+            <<i+1<<"      "
             <<P[i].at<<"    "
-            <<P[i].bt<<"    "
-            <<P[i].pbt<<endl;
+            <<P[i].bt<<"     "
+            <<P[i].pbt<<"    "
+            <<P[i].ct<<"      "
+            <<P[i].tat<<"     "
+            <<P[i].wt<<"    "<<endl;
 }
 
 int main()
 {
-    int N=0, timE=0, a=0, pid=0, sum=0;
-    double atat=0.0, awt=0.0;
+    int N=0, timE=0, a=0, pid=0;
+    double atat=0.0, awt=0.0, alpha=0.5;
     
     cout<<"\n\nEnter number of process: ";
     cin>>N;
@@ -32,11 +58,11 @@ int main()
     {
         cout<<"Enter arrival and burst time for proceess with pid as "<<i+1<<" :";
         cin>>P[i].at>>P[i].bt;
-        P[i].pbt = 32000;
         P[i].ct = P[i].tat = P[i].wt = 0;
         P[i].isCompleted = false;
     }
-
+    
+    
     //Sort according to arrival time
     for(int i=0; i<N; ++i)
         for(int j=i; j<N; ++j)
@@ -47,11 +73,32 @@ int main()
                 P[j+1] = temp;
             }
     
-    for(int i=0; i<N; ++i)
+    //Setting predicted burst time
+    P[0].pbt = P[0].bt;
+    for(int i=1; i<N; ++i)
+        P[i].pbt = alpha*P[i-1].bt + (1-alpha)*P[i-1].pbt;
+    
+    cout<<"\nGantt chart:\n| ";
+    do
     {
-        P[i].pbt = sum/(i+1);
-        sum += P[i].bt;
-    }
+        pid = getShotestArrivedProcess(P, N, timE);
+        if(pid != -1) timE += P[pid].bt;
+        else
+        {  
+            cout<<++timE<<" | ";
+            continue;
+        }
+        P[pid].ct = timE;
+        P[pid].isCompleted = true;
+        P[pid].tat = P[pid].ct - P[pid].at;
+        P[pid].wt = P[pid].tat - P[pid].bt;
+        atat += P[pid].tat;
+        awt += P[pid].wt;
+        cout<<"P"<<pid+1<<" | ";
+    }while(isProcessRemaining(P, N));
+    
     showProcessTab(P, N);
+    cout<<"\nAverage turaroung time: "<<atat/N
+        <<"\nAverage Waiting time: "<<awt/N<<endl;
     return 0;
 }
